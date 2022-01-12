47accd26efdcfb53cdf4e0efb21ea15c6de6a5b9
202-09\User <hi1856@naver.com>
2022-01-12 15:46:49 +0900

    2022-01-12 수업내용1

--- src/main/java/com/test/quiz/DatabaseQuiz01.java
+++ src/main/java/com/test/quiz/DatabaseQuiz01.java
@@ -16,30 +16,41 @@ import com.test.common.MysqlService;
 public class DatabaseQuiz01 extends HttpServlet {
 
 	@Override
-	public void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException {
+	public void doGet (HttpServletRequest request, HttpServletResponse response) {
 		response.setContentType("text/plain");
 		
 		// DB연결
 		MysqlService mysqlService = MysqlService.getInstance();
 		mysqlService.connection();
 		
-			//insert 쿼리
-			String insertQuery = "insert into `real_estate`(`realtorld`, `address`, `area`, `type`, `price`, `rentPrice`)"
-					+ "values (3, '헤라펠리스 101ehd 5305호', 350, `매매`, 15000, null)";
-		// Select 쿼리 
-		String selectQuery = "select * from `real_estate`";
-		PrintWriter out = response.getWriter();
 		try {
-			ResultSet resultSet = mysqlService.select(selectQuery);
+		//insert 쿼리
+		String insertQuery = "insert into `real_estate`(`realtorld`, `address`, `area`, `type`, `price`, `rentPrice`)"
+				+ "values (3, '헤라펠리스 101ehd 5305호', 350, `매매`, 1500000, null)";
+		
+			mysqlService.update(insertQuery);
+		
+		// Select 쿼리 -> 처리(출력)
+		String selectQuery = "select * from `real_estate` order by `id` desc limit 10";
+		ResultSet resultSet = mysqlService.select(selectQuery);
 			
+		PrintWriter out = response.getWriter();
+		
 			while (resultSet.next()) {
-				out.println("매물정보: ");
+				String address = resultSet.getString("address");
+				int area = resultSet.getInt("area");
+				String type = resultSet.getString("type");
+				
+				out.println("매물정보: " + address + ", 면적:" + area + ", 타입:" + type);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} catch (IOException ioe) { // PrintWriter을 내가 처리하는 방법을 사용한것!!
+			ioe.printStackTrace();
 		}
 		
-		
+		// DB 연결 해제
+		mysqlService.disconneciton();
 		
 	}
 }
